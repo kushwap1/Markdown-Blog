e12afb85292520dd73f52ebd0db55f353e2461e7
Pankaj Kushwaha <59306586+kushwap1@users.noreply.github.com>
2021-01-21 00:36:55 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const articleRouter = require('./routes/articles')
 const methodOverride = require('method-override')
 const app = express()
 
-mongoose.connect('mongodb://localhost/blog', {
+mongoose.connect('mongodb://some-mongo/blog', {
   useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true
 })
 
